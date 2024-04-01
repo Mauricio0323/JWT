f51989286359141d561357aa41f55cf5cf966477
Mauricio0323 <136137023+Mauricio0323@users.noreply.github.com>
2024-03-31 22:49:00 -0500

    first commit

---
+++ Back/src/constants.js
@@ -0,0 +1,7 @@
+export const DB_HOST = "crm.5ylg3wz.mongodb.net";
+export const DB_USER = "CrmAdmin";
+export const DB_PASS = "Tsj38xAtlDsNrzf0";
+export const DB_NAME = "CRM";
+export const API_VERSION = "v1";
+export const IP_SERVER = "localhost";
+export const PORT = 9005;
