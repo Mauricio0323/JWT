656add2c9b2cc3e20f326fa82018641179588029
Mauricio0323 <136137023+Mauricio0323@users.noreply.github.com>
2024-04-03 15:34:46 -0500

    crear token para login y register

--- Back/src/models/user.model.js
+++ Back/src/models/user.model.js
@@ -1,26 +1,27 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema(
 
-    username: {
-        type: String,
-        require: true,
-        trim: true
-
-    },
-    email: {
-        type: String,
-        require: true,
-        trim: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        require: true,
-    },
-
-}, {
-    timestamps: true
-})
-
-export default mongoose.model('User', UserSchema)
+    {
+        username: {
+          type: String,
+          required: true,
+          trim: true,
+        },
+        email: {
+          type: String,
+          required: true,
+          trim: true,
+          unique: true,
+        },
+        password: {
+          type: String,
+          required: true,
+        },
+      },
+      {
+        timestamps: true,
+      }
+    );
+    
+    export default mongoose.model("User", UserSchema);
